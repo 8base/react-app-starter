319b568fcd49a4bcd930ed44eb0582538c68dd9e
ej9x <strcmp9@gmail.com>
2019-03-25 15:51:16 +0300

    add fields common components

--- src/Root.js
+++ src/Root.js
@@ -4,16 +4,20 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { ProtectedRoute } from './shared/components';
 import { MainPlate, ContentPlate, Nav } from './components';
 import { Auth } from './routes/auth';
+/** APP_PAGES_IMPORTS */
 
 export const Root = () => (
   <Switch>
     <Route path="/auth" component={Auth} />
     <Route>
       <MainPlate>
-        <Nav.Plate color="BLUE" />
+        <Nav.Plate color="BLUE">
+          {/** APP_ROUTE_LINKS */}
+        </Nav.Plate>
         <ContentPlate>
           <Switch>
-          </Switch>
+            {/** APP_ROUTES */}
+        </Switch>
         </ContentPlate>
       </MainPlate>
     </Route>
--- src/shared/components/FileInputField.js
+++ src/shared/components/FileInputField.js
@@ -1,28 +1,20 @@
 import React from 'react';
-// import { FileInput } from '@8base/file-input';
+import { FileInput } from '@8base/file-input';
 import { Button, Form, Row, Text } from '@8base/boost';
 
 export const FileInputField = ({ input, meta, maxFiles, label, ...rest }) => (
   <Form.Field label={label} input={input} meta={meta}>
-    <div />
-    {/* <FileInput onChange={input.onChange} value={input.value} maxFiles={maxFiles} public={rest.public}>
+    <FileInput onChange={input.onChange} value={input.value} maxFiles={maxFiles} public={rest.public}>
       {({ pick, value }) => (
         <Row stretch alignItems="center">
-          <Button
-            type="button"
-            onClick={() => {
-              pick();
-            }}
-            stretch
-            color="neutral"
-          >
+          <Button type="button" onClick={() => pick()} stretch color="neutral">
             Choose Files
           </Button>
-          <Text size="sm" style={{ whiteSpace: 'nowrap' }}>
+          <Text size="sm">
             {value ? (Array.isArray(value) ? `${value.length} files selected` : value.filename) : 'No files selected'}
           </Text>
         </Row>
-      )} 
-    </FileInput>*/}
+      )}
+    </FileInput>
   </Form.Field>
 );
